e8e0f4e8296b961dfe9d2d396b7bd05a70b370a3
MariuszZiental <122224840+MariuszZiental@users.noreply.github.com>
2023-09-02 10:03:07 +0200

    hw10 second commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.headers.common['x-api-key'] = 'твой ключ';
+axios.defaults.headers.common['x-api-key'] =
+  'live_Gm1KT3TtrkxZxkXwjeq2qX4yOlDiff82A0Ffni8FFMit3KT4upGHMDXBOCJPfCYq';
+
+  
